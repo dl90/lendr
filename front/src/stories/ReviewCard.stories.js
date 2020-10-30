7e9d6b2f3c9727ab43137d45b5a1ee05af14f3e0
= <=>
2020-10-29 20:44:23 -0700

    Created the review card

---
+++ front/src/stories/ReviewCard.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReviewCard from '../comps/ReviewCard';
+
+export default {
+    title: 'Review Card',
+    component: ReviewCard,
+};
+
+
+
+export const ReviewerCard = () => <ReviewCard />
