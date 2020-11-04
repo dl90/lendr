c65f6a80118b57ba45d440202be11b18b7d93944
Don <donli90@hotmail.com>
2020-11-03 20:21:04 -0800

    fix

--- server.js
+++
@@ -1,5 +0,0 @@
-import app from './app.js'
-import db from './db/db.js'
-
-db.connect(err => { if (err) console.log(err) })
-app(db).listen(8000, () => console.log(`Example app listening at http://localhost:${8000}`))
