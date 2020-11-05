df92919eb2772d390f16c98918c9ffa3644d9ee5
= <=>
2020-11-05 15:01:51 -0800

    Finished Renters profile

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // import './App.css';
-// import Pages from "./pages/renterProfile";
+import Pages from "./pages/renterProfile";
 // import Pages from "./pages/index";
 // import Pages from "./pages/post";
 // import Pages from "./pages/login";
 // import Pages from "./pages/signUp";
-import Pages from "./pages/item";
+// import Pages from "./pages/item";
 
 function App() {
   return (
--- front/src/pages/renterProfile.js
+++ front/src/pages/renterProfile.js
@@ -1,32 +1,84 @@
 import React from 'react';
-import BottomNav from '../comps/BottomNav';
+import './app.scss';
+import './renterProfile.scss';
+
+import UploadImg from '../comps/UploadImg';
+
 import CategoryGallery from '../comps/CategoryGallery';
 import Header from '../comps/Header';
-import './renterProfile.scss';
-import ProfileCard from '../comps/ProfileCard'
+import ProfileCard from '../comps/ProfileCard';
 import ReviewCard from '../comps/ReviewCard';
-import './app.scss'
 
+import BottomNav from '../comps/BottomNav';
 
 export default function Home() {
     return <div className="app">
         <Header />
 
         <ProfileCard msgState={true} />
-        <h2>Chads Items</h2>
-        <div className="column01">
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-        </div>
-        <h2>Successful Lends</h2>
-        <div className="column01">
-            <ReviewCard />
 
+
+        <div className="imageDiv">
+            <h2>Lenders Items</h2>
+            <div className="images">
+                <ReviewCard />
+                <ReviewCard />
+                <ReviewCard />
+                <ReviewCard />
+                <ReviewCard />
+                <ReviewCard />
+                <ReviewCard />
+            </div>
         </div>
+
+
+        <div className="imageDiv">
+            <h2>Successful Lends</h2>
+            <div className="images">
+                <ReviewCard />
+                <ReviewCard />
+            </div>
+        </div>
+
         <div className="nav">
             <BottomNav />
         </div>
     </div>
 }
+
+
+// export default function Home() {
+//     return <div>
+//         <div className="post">
+//             <Header />
+//         </div>
+
+
+//         <div className="imageDiv">
+//             <h2>Lenders Items</h2>
+//             <div className="images">
+//                 <ReviewCard />
+//                 <ReviewCard />
+//                 <ReviewCard />
+//                 <ReviewCard />
+//                 <ReviewCard />
+//                 <ReviewCard />
+//                 <ReviewCard />
+//             </div>
+//         </div>
+
+
+{/* <div className="post">
+            <CategoriesDropdown />
+            <InputParagraph title={"Description"} placeholder="Write a description of the item you're renting out."></InputParagraph>
+            <Input title={"Meeting Location"} placeholder="123 Main St."></Input>
+            <div className="button">
+                <Button text={"Post"}></Button>
+            </div>
+        </div> */}
+
+{/* <div className="nav">
+    <BottomNav />
+</div>
+    </div >
+} */}
