d03f24e8fdf577b24f33381960a340a1c5b86565
= <=>
2020-11-04 23:15:52 -0800

    changed up some CSS for a comp

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import './App.css';
-import Pages from "./pages/renterProfile";
+
+import Pages from "./pages/messages";
+// import Pages from "./pages/renterProfile";
+// import Pages from "./pages/index";
 
 function App() {
   return (
