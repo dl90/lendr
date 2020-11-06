12118983c7e547237e5f29b054bddd6504eea7a9
= <=>
2020-11-06 12:38:20 -0800

    made the settings page

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import './App.css';
-import Pages from "./pages/renterProfile";
+// import Pages from "./pages/renterProfile";
 // import Pages from "./pages/index";
 // import Pages from "./pages/post";
 // import Pages from "./pages/login";
 // import Pages from "./pages/signUp";
 // import Pages from "./pages/item";
+import Pages from "./pages/settings";
 
 function App() {
   return (
