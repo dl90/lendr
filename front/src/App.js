612c790d2e5dea84fb9b02d864601053fbc4f624
luudavid778 <luudavid778@gmail.com>
2020-11-10 10:38:26 -0800

    Merged changes

--- front/src/App.js
+++ front/src/App.js
@@ -4,11 +4,13 @@ import React from 'react';
 // import Pages from "./pages/index";
 // import Pages from "./pages/post";
 // import Pages from "./pages/login";
-import Pages from "./pages/signUp";
+// import Pages from "./pages/signUp";
 // import Pages from "./pages/item";
 // import Pages from "./pages/edit";
 // import Pages from "./pages/settings";
 // import Pages from "./pages/messages";
+import Pages from './pages/explorepage';
+
 
 
 function App() {
