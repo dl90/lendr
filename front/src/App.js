99699c163fd377df92094d6459e4135d7d1e8079
Joseph Li <College@Gammas-iMac.local>
2020-11-26 08:54:01 -0800

    12

--- front/src/App.js
+++ front/src/App.js
@@ -41,7 +41,7 @@ const App = () => {
 
   return (
     <Router>
-      <LogoPage />
+      {/* <LogoPage /> */}
       <div>
 
 
--- front/src/pages/lendingHistory.js
+++ front/src/pages/lendingHistory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BottomNav from '../comps/BottomNav';
 import CategoryGallery from '../comps/CategoryGallery';
 import Header from '../comps/Header';
@@ -7,22 +7,47 @@ import './app.scss';
 import SearchBar from '../comps/SearchBar';
 import MyLendsButtons from '../comps/MyLendsButtons';
 import ItemCard from '../comps/ItemCard';
-
-
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 
 
 export default function LendingHistory() {
+    const [Items, setItems] = useState([]);
+
+    const HandleGetItems = async () => {
+        var itemresp = await axios.post("https://www.lendr-bc.me/post/get-all-own", {idx: 0, count: 5}, {
+            headers: { crossDomain: true, 'Content-Type': 'application/json' }
+        }, { withCredentials: true });
+        console.log("repo data");
+        console.log(itemresp.data);
+
+        setItems([...itemresp.data]);
+    }
+
+    useEffect(() => {
+        HandleGetItems();
+    }, [])
+
     return <div className="app">
         <Header />
-        <h1>My History</h1>
+        <h1>Available</h1>
         {/* <Link to="/post">Post</Link> */}
         <div className="search">
             <MyLendsButtons />
         </div>
-        <ItemCard />
-        <ItemCard />
+        <div className="items">
+            {
+                Items.map((o,i)=>{
+                    console.log("inside the array...", o,i);
+                    return <ItemCard
+                    img={o.images}
+                    itemname={o.title}
+                    price={o.rate}
+                    />
+                })
+            }
+        </div>
         <div className="nav">
             <BottomNav active={2} />
         </div>
